71175d2c93444f9973f53d06053a16b8f5a5ccf9
Anuar <ava200ava@gmail.com>
2023-09-04 21:03:28 +0600

    first commit

---
+++ src/components/mexcChart.tsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+//
+// const MexcChart = () => {
+//     return (
+//         <div>
+//             <h1>MEXC</h1>
+//         </div>
+//     );
+// };
+//
+// export default MexcChart;
